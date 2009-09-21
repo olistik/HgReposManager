35895a3fac2a91d4da19a147223583e968fa2b3f
Maurizio De Magnis <maurizio.demagnis@gmail.com>
2009-09-21 07:43:44 +0200

    add logout button

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   map.login 'login', :controller => :user_sessions, :action => :new
+  map.logout 'logout', :controller => :user_sessions, :action => :destroy
   map.register 'register', :controller => :users, :action => :new
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
